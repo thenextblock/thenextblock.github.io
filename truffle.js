9602c3cb24ad5349fb24ec83b4245c755bc080eb
Archil Phanchulidze <achiko@gmail.com>
2018-02-22 19:24:27 +0400

    truffle config added

--- truffle.js
+++ truffle.js
@@ -1,4 +1,32 @@
+require('babel-register')
+require('babel-polyfill')
+
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "577", // Match any network id
+      gas: 4712388
+    },
+    ganache: {
+      host: "localhost",
+      port: 8547,
+      network_id: "577", // Match any network id
+      gas: 4712388
+    }
+  }
+  // ,mocha: {
+  //   reporter: 'eth-gas-reporter',
+  //   reporterOptions : {
+  //     currency: 'USD',
+  //     gasPrice: 50,
+  //     outputFile : 'gas-report.md'
+  //   }
+  // }
+  
 };
+
+
+
